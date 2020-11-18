db37d2ef1080ba2cbfbe77c07e44b98f337a1195
Ahad Shabbir <shabbirahadh@gmail.com>
2020-11-18 00:16:59 -0600

    added submodule update in build script

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# Keep ddcbc-api submodule update
+git submodule update --remote
 gcc -g -pthread `pkg-config --cflags gtk+-3.0` -o ddcbc-gtk main.c `pkg-config --libs gtk+-3.0` -lddcutil
